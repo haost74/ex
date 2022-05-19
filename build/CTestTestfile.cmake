87e478183969ee92ca49231507411670abff49a5
haost74 <haost@yandex.ru>
2022-05-19 21:32:56 +0300

    clear

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/74hao/Desktop/ex
-# Build directory: C:/Users/74hao/Desktop/ex/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/74hao/Desktop/ex/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- build/hello_test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[HelloTest.BasicAssertions]=]  C:/Users/74hao/Desktop/ex/build/Debug/hello_test.exe [==[--gtest_filter=HelloTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[HelloTest.BasicAssertions]=]  PROPERTIES WORKING_DIRECTORY C:/Users/74hao/Desktop/ex/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  hello_test_TESTS HelloTest.BasicAssertions)
