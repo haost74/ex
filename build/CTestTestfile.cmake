68b13642fba5e648f35146ff8c729322c6d9b546
haost74 <haost@yandex.ru>
2022-05-19 21:08:01 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/haost/Desktop/ex
+# Build directory: /home/haost/Desktop/ex/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/haost/Desktop/ex/build/hello_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
